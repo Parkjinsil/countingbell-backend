d4e227785eaeffd11376d39915faa3d5472ff9e1
wlstlf971017 <wlstlf971017@naver.com>
2023-09-19 14:30:14 +0900

    0919리셋

--- src/main/java/com/kh/countingBell/repo/MemberDAO.java
+++
@@ -1,7 +0,0 @@
-package com.kh.countingBell.repo;
-
-import com.kh.countingBell.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberDAO extends JpaRepository<Member, String> {
-}
--- src/main/java/com/kh/countingBell/repo/ReviewDAO.java
+++
@@ -1,8 +0,0 @@
-package com.kh.countingBell.repo;
-
-import com.kh.countingBell.domain.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewDAO extends JpaRepository<Review,Integer> {
-
-}
