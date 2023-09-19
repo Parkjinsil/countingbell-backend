15471d49b1e880208e1c5d9a789708709e5a2d13
JunHyeokChoi1 <wnsgure3@gmail.com>
2023-09-19 14:44:44 +0900

    feat

---
+++ src/main/java/com/kh/countingBell/controller/ResCommentController.java
@@ -0,0 +1,54 @@
+package com.kh.countingBell.controller;
+
+import com.kh.countingBell.domain.ResComment;
+import com.kh.countingBell.service.ResCommentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/*")
+public class ResCommentController {
+
+    @Autowired
+    private ResCommentService resComment;
+
+    @GetMapping("/comment")
+    public ResponseEntity<List<ResComment>> showResCommentList() {
+        return ResponseEntity.status(HttpStatus.OK).body(resComment.showAllResComment());
+    }
+
+    @GetMapping("/comment/{id}")
+    public ResponseEntity<ResComment> showResComment(@PathVariable int id) {
+        return ResponseEntity.status(HttpStatus.OK).body(resComment.showResComment(id));
+    }
+
+    @PostMapping("/comment")
+    public ResponseEntity<ResComment> createResComment(@RequestBody ResComment vo){
+        return ResponseEntity.status(HttpStatus.OK).body(resComment.createResComment(vo));
+    }
+
+    @PutMapping("/comment")
+    public ResponseEntity<ResComment> updateResComment(@RequestBody ResComment vo){
+        ResComment result = resComment.updateResComment(vo);
+        if(result!=null) {
+            return ResponseEntity.status(HttpStatus.OK).body(result);
+        }
+        return  ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @DeleteMapping("/comment/{id}")
+    public ResponseEntity<ResComment> deleteResComment(@PathVariable int id){
+        return ResponseEntity.status(HttpStatus.OK).body(resComment.deleteResComment(id));
+    }
+
+    // 식당에 따른 식당 댓글 조회
+
+
+
+}
---
+++ src/main/java/com/kh/countingBell/domain/ResComment.java
@@ -0,0 +1,43 @@
+package com.kh.countingBell.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@DynamicInsert
+@Table(name="RES_COMMENT")
+
+public class ResComment {
+
+    @Id
+    @Column(name="res_comment_code")
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "ResCommentSequence")
+    @SequenceGenerator(name="ResCommentSequence", sequenceName = "SEQ_RES_COMMENT", allocationSize = 1)
+    private int resCommentCode;
+
+    @Column(name="res_comment_detail")
+    private String resCommentDetail;
+
+    @Column(name="res_comment_date")
+    private Date resCommentDate;
+
+    @Column(name="res_parent_comment")
+    private String resParentComment;
+
+//    @ManyToOne
+//    @JoinColumn(name="id")
+//    private Member member;
+//
+//    @ManyToOne
+//    @JoinColumn(name="review_code")
+//    private Review review;
+
+}
