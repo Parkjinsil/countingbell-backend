29b57a6934c9e0fb63f8a75ee69b1755c99635c8
nimoos9480 <soomin6372@gmail.com>
2023-10-23 00:08:07 +0900

    feat_아이디찾기 추가

--- src/main/java/com/kh/countingBell/controller/MenuController.java
+++ src/main/java/com/kh/countingBell/controller/MenuController.java
@@ -53,12 +53,12 @@ public class MenuController {
         Pageable pageable = PageRequest.of(page-1, 10, sort);
 
         Page<Menu> result = menuService.showAll(pageable);
-        log.info("Total Pages : " + result.getTotalPages()); // 총 몇 페이지
-        log.info("Total Count : " + result.getTotalElements()); // 전체 개수
-        log.info("Page Number : " + result.getNumber()); // 현재 페이지 번호
-        log.info("Page Size : " + result.getSize()); // 페이지당 데이터 개수
-        log.info("Next Page : " + result.hasNext()); // 다음 페이지가 있는지 존재 여부
-        log.info("First Page : " + result.isFirst()); // 시작 페이지 여부
+//        log.info("Total Pages : " + result.getTotalPages()); // 총 몇 페이지
+//        log.info("Total Count : " + result.getTotalElements()); // 전체 개수
+//        log.info("Page Number : " + result.getNumber()); // 현재 페이지 번호
+//        log.info("Page Size : " + result.getSize()); // 페이지당 데이터 개수
+//        log.info("Next Page : " + result.hasNext()); // 다음 페이지가 있는지 존재 여부
+//        log.info("First Page : " + result.isFirst()); // 시작 페이지 여부
 
 
         return ResponseEntity.status(HttpStatus.OK).body(result.getContent());
@@ -81,10 +81,10 @@ public class MenuController {
                                            @RequestPart(value = "menuPicture", required = true) MultipartFile menuPicture,
                                            @RequestParam(value = "menuName", required = true) String menuName,
                                            @RequestParam(value = "menuPrice", required = true) String menuPrice) {
-        log.info("menuPicture : " + menuPicture);
-        log.info("menuName : " + menuName);
-        log.info("menuPrice : " + menuPrice);
-        log.info("resCode : " + resCode);
+//        log.info("menuPicture : " + menuPicture);
+//        log.info("menuName : " + menuName);
+//        log.info("menuPrice : " + menuPrice);
+//        log.info("resCode : " + resCode);
 
         String originalPhoto = menuPicture.getOriginalFilename();
         String realImage = originalPhoto.substring(originalPhoto.lastIndexOf("\\")+1);
@@ -122,8 +122,10 @@ public class MenuController {
                                            @RequestPart(value = "menuPicture", required = true) MultipartFile menuPicture,
                                            @RequestParam(value = "menuName", required = true) String menuName,
                                            @RequestParam(value = "menuPrice", required = true) String menuPrice) {
-        String originalPhoto = menuPicture.getOriginalFilename();
-        String realImage = originalPhoto.substring(originalPhoto.lastIndexOf("\\")+1);
+
+
+        String newPhoto = menuPicture.getOriginalFilename();
+        String realImage = newPhoto.substring(newPhoto.lastIndexOf("\\")+1);
         String uuid = UUID.randomUUID().toString();
         String savePhoto = uploadPath + File.separator + uuid + "_" + realImage;
         Path pathPhoto = Paths.get(savePhoto);
@@ -144,9 +146,7 @@ public class MenuController {
         restaurant.setResCode(resCode);
         vo.setRestaurant(restaurant);
 
-
-
-        return ResponseEntity.status(HttpStatus.OK).body(menuService.create(vo));
+ return ResponseEntity.status(HttpStatus.OK).body(menuService.create(vo));
     }
 
     //    http://localhost:8080/api/menu/{id}
--- src/main/java/com/kh/countingBell/repo/MemberDAO.java
+++ src/main/java/com/kh/countingBell/repo/MemberDAO.java
@@ -1,9 +1,18 @@
 package com.kh.countingBell.repo;
 
 import com.kh.countingBell.domain.Member;
+import com.kh.countingBell.domain.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberDAO extends JpaRepository<Member, String> {
+public interface MemberDAO extends JpaRepository<Member, String>, QuerydslPredicateExecutor<Member> {
+
+    @Query(value = "SELECT id FROM member WHERE name=:name AND phone=:phone", nativeQuery = true)
+    String searchId(String name, String phone);
+
+//    @Query(value = "SELECT password FROM member WHERE id=:id AND phone=:phone", nativeQuery = true)
+//    String searchPwd(String name, String phone);
 }
