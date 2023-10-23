b39bf88dac14eae921abed3e22e7497e892b4d70
nimoos9480 <soomin6372@gmail.com>
2023-10-23 22:41:10 +0900

    feat_아이디찾기 @Param값 명시

--- src/main/java/com/kh/countingBell/repo/MemberDAO.java
+++ src/main/java/com/kh/countingBell/repo/MemberDAO.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Repository;
 public interface MemberDAO extends JpaRepository<Member, String>, QuerydslPredicateExecutor<Member> {
 
     @Query(value = "SELECT id FROM member WHERE name=:name AND phone=:phone", nativeQuery = true)
-    String searchId(String name, String phone);
+    String searchId(@Param("name")String name, @Param("phone")String phone);
 
 //    @Query(value = "SELECT password FROM member WHERE id=:id AND phone=:phone", nativeQuery = true)
 //    String searchPwd(String name, String phone);
